e617d161fbb201cdcd6e138a53c1c60a637007b9
yoen <jendral.hxr@gmail.com>
2020-10-18 11:04:33 +0900

    clip-cut videos

--- whatevering/image2video.py
+++ whatevering/image2video.py
@@ -1,16 +1,17 @@
 import cv2
 import numpy as np
 import glob
+import sys
  
 img_array = []
-for filename in glob.glob('D:/getar/p1540/*.png'):
+for filename in glob.glob(sys.argv[1]+'/*.png'):
     img = cv2.imread(filename)
     print(filename)
     height, width, layers = img.shape
     size = (width,height)
     img_array.append(img)
     
-out = cv2.VideoWriter('D:/getar/p1540.mp4',cv2.VideoWriter_fourcc(*'MP4V'), 60, size)
+out = cv2.VideoWriter(sys.argv[2],cv2.VideoWriter_fourcc(*'MP4V'), 60, size)
  
 for i in range(len(img_array)):
     out.write(img_array[i])
--- whatevering/video2image.py
+++ whatevering/video2image.py
@@ -8,13 +8,20 @@ start= int(sys.argv[2])
 stop = int(sys.argv[3])
 n = start
 
+startx= 1600
+starty=600
+stopx= 2400
+stopy= 1640
+
 #os.makedirs(dir_path, exist_ok=True)
 #base_path = os.path.join(dir_path, basename)
 
 digit = len(str(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))))
 while n < stop:
     ret, frame = cap.read()
-    cv2.imwrite('{}/{}.png'.format(sys.argv[4], str(n).zfill(digit)), frame)
+    cropped = frame[starty:stopy, startx:stopx]
+    cv2.imwrite('{}/{}.png'.format(sys.argv[4], str(n).zfill(digit)), cropped)
+    #cv2.imwrite('{}/{}.png'.format(sys.argv[4], str(n).zfill(digit)), frame)
     n += 1
     print(str(n).zfill(digit))
     
