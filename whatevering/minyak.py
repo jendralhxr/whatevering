18db686fe0e6f7bee45014862c796a0c585eb694
yoen <jendral.hxr@gmail.com>
2021-06-11 18:11:19 +0900

    single line detection actually works much better

--- whatevering/minyak.py
+++ whatevering/minyak.py
@@ -24,10 +24,13 @@ step= 10;
     
 framenum= 0
 ret, frame = cap.read()
-prev= frame;
+crop= frame[y_start:y_stop, 0:frame_width];
+prev= crop;
+cue=  crop;
 
 edge_left= [0] * y_span;
 edge_right= [0] * y_span;
+midline= int (y_span/2);
 
 while(1):
     ret, frame = cap.read()
@@ -36,51 +39,30 @@ while(1):
     crop= frame[y_start:y_stop, 0:frame_width];
     ret,cue = cv2.threshold(crop, threshold_bin, 255, cv2.THRESH_BINARY);
 	
-    # assign existing ID, either from current or neighboring pixel in previous cue
-    for j in range(0, y_span):
-        for i in range(detection_line+1, frame_width):
-            pix_jk= 0;
-            if (cue.item(j,i,1) == 255):
-                # ID from previous frame
-                if (prev.item(j,i,1) != 255) and (prev.item(j,i,1) != 0):
-                #    cue.itemset((j,i,1), prev.item(j,i,1));
-                    pix_jk= 1;
-                # ID from neighboring pixel
-                #elif (cue.item(j,i-1,0) != 255) and (cue.item(j,i-1,0) != 0):
-                #    cue.itemset((j,i,0), cue.item(j,i-1,0));
-                #    pix_jk= 1;
-                # blank it out if no ID is assigned    
-                #if pix_jk==0:
-                else:
-                    pix_jk=0;
-                    #cue.itemset((j,i,1), 0);
-                
-                
-    # find blob edges
-    for j in range(0, y_span):
-        # find left edge
-        for i in range(detection_line, detection_line-2*radius, -1):
-            if (cue.item(j,i,0) == 0):
-                edge_left[j]= i;
-                break;
-        # find right edge
-        for i in range(detection_line, detection_line+2*radius):
-            if (cue.item(j,i,0) == 0):
-                edge_right[j]= i;
-                break; 
-    
-    # fill in the blob if convex meniscus
-    blob_id= random.randint(30, 230)
-    #blob_id= 50;
-    belt_cen= edge_right[y_mid-y_start] - edge_left[y_mid-y_start];
-    belt_lat= edge_right[y_mid-y_start-step] - edge_left[y_mid-y_start-step];
-    #print("{} {} {} {}".format(edge_right[y_mid-y_start], edge_left[y_mid-y_start], edge_right[y_mid-y_start-10], edge_left[y_mid-y_start-10] ));
-    if (belt_cen > radius) and (belt_cen > belt_lat):
-        print("{} {}".format(belt_cen, belt_lat));
-        for j in range(0, y_span):
-            for i in range(edge_left[j], edge_right[j]):
-                if (cue.item(j,i,1) == 255):
-                    cue.itemset((j,i,1), blob_id);
+    print("---------------");
+    blob_start= detection_line;
+    blob_end= detection_line;
+    for x in range (0, frame_width-1):
+        # measure blob width
+        if (cue.item(midline, x, 1) == 255) and (blob_start == blob_end):
+            blob_start= x;
+        if (cue.item(midline, x, 1) == 0) and (blob_end != blob_start):
+            blob_end= x;
+            new_id= random.randint(30, 230);
+            old_id= prev.item( midline, int((blob_end+blob_start)/2)+2, 1);
+            # apply old ID if possible
+            if (old_id != 255) and (old_id != 0):
+                print("{} {} {}". format(blob_start, blob_end, old_id));
+                for blob_x in range (blob_start, blob_end):
+                    cue.itemset((midline, blob_x, 1), old_id);
+                # else, assign new ID
+            elif (detection_line > blob_start) and (detection_line < blob_end):  
+                #print("{} {} {}". format(blob_start, blob_end, new_id));
+                for blob_x in range (blob_start, blob_end):
+                    cue.itemset((midline, blob_x, 1), new_id);
+            blob_start= blob_end;    
+
+    # apply the watersehd
     
     prev= cue;
     
