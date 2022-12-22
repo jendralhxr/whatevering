8c718efdf54ef594d7e0c1dd98092058cb3f3141
yoen <jendral.hxr@gmail.com>
2022-12-22 17:49:15 +0900

    make it fancy!

--- multicrop.py
+++ multicrop.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-# python video2fps.py input.mp4 output.mp4 startframe endframe framestoskip
-# 00000.MTS starts at 400
-# 00001.MTS starts at 0
+# python multicrop.py input.mp4 output1.mp4 output2.mp4 output3.mp4 lastframe
 
 import numpy as np
 import math
@@ -14,17 +12,17 @@ from datetime import datetime
 traffic_start_x= 0;
 traffic_start_y= 0;
 traffic_end_x= 4096;
-traffic_end_y= 280;
+traffic_end_y= 232;
 
 deck_start_x= 0;
-deck_start_y= 312;
+deck_start_y= 246;
 deck_end_x= 4096;
-deck_end_y= 496;
+deck_end_y= 334;
 
-girder_start_x= 482;
-girder_start_y= 550;
-girder_end_x= 3130;
-girder_end_y= 774;
+girder_start_x= 0;
+girder_start_y= 428;
+girder_end_x= 4096;
+girder_end_y= 500;
 
 FRAME_STEP= 1;
 
@@ -33,13 +31,20 @@ deck_size = (deck_end_x-deck_start_x, deck_end_y-deck_start_y)
 girder_size = (girder_end_x-girder_start_x, girder_end_y-girder_start_y)
 
 cap = cv2.VideoCapture(sys.argv[1])
-out_traffic = cv2.VideoWriter(sys.argv[2],cv2.VideoWriter_fourcc(*'mp4v'), 100.0, traffic_size)
-out_deck = cv2.VideoWriter(sys.argv[3],cv2.VideoWriter_fourcc(*'mp4v'), 100.0, deck_size)
-out_girder = cv2.VideoWriter(sys.argv[4],cv2.VideoWriter_fourcc(*'mp4v'), 100.0, girder_size)
+if not cap.isOpened():
+    sys.exit()
+else:
+    # Video Parameters
+    IM_SIZE_Y = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH));
+    IM_SIZE_X = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT));
+    CameraFrameRate = 100;
+    length = cap.get(cv2.CAP_PROP_FRAME_COUNT);
+    print("video is {}x{} px @{} fps for {} frames".format(IM_SIZE_Y, IM_SIZE_X, CameraFrameRate, length))
+    out_traffic = cv2.VideoWriter(sys.argv[2],cv2.VideoWriter_fourcc(*'mp4v'), 100.0, traffic_size)
+    out_deck = cv2.VideoWriter(sys.argv[3],cv2.VideoWriter_fourcc(*'mp4v'), 100.0, deck_size)
+    out_girder = cv2.VideoWriter(sys.argv[4],cv2.VideoWriter_fourcc(*'mp4v'), 100.0, girder_size)
 
 framenum = int(sys.argv[5])
-length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-print( "length of video: " + str(length) )
 
 digit=6
 
