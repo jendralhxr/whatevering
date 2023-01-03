2ecff4559db34e5c21f3d96c79efa846ed5da7db
yoen <jendral.hxr@gmail.com>
2023-01-03 16:18:00 +0900

    hate those U+00A0
    guilty of copy-pasting the lines from quora :)

--- appendcol.py
+++ appendcol.py
@@ -1,16 +1,12 @@
+#use it like:
+#  for i in *csv; do python -u appendcol.py out.csv $i; done
+
 import pandas as pd
 import sys
- 
-# Set path to CSV files 
-path = sys.argv[1])
- 
-# Read CSV file into DataFrame 
-output = pd.read_csv(sys.argv[1])) 
-source = pd.read_csv(sys.argv[2])) 
- 
-# Transfer column 'x' from df1 to df2 
-output = pd.concat([output, source['33_vx']], axis=1) 
-output.rename(columns={'33_vx':source})
 
-# Save df2 as CSV 
+path = sys.argv[1]
+output = pd.read_csv(sys.argv[1])
+source = pd.read_csv(sys.argv[2])
+output = pd.concat([output, source['33_vx']], axis=1)
+output.rename(columns={'33_vx':source})
 output.to_csv(sys.argv[1])
