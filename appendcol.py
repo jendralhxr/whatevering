64940f34e476be9f2e2ff395c4ae36307077be60
yoen <jendral.hxr@gmail.com>
2023-01-03 16:43:44 +0900

    make in neat
    removed the index in the output file

--- appendcol.py
+++ appendcol.py
@@ -1,5 +1,5 @@
 #use it like:
-#  for i in *csv; do python -u appendcol.py out.csv $i; done
+#  for i in *csv; do python -u appendcol.py compiled.csvx $i; done
 
 import pandas as pd
 import sys
@@ -7,6 +7,6 @@ import sys
 path = sys.argv[1]
 output = pd.read_csv(sys.argv[1])
 source = pd.read_csv(sys.argv[2])
-output = pd.concat([output, source['33_vx']], axis=1)
-output.rename(columns={'33_vx':source})
-output.to_csv(sys.argv[1])
+temp= value=source['33_vx']
+output.insert(loc=len(output.columns), column=sys.argv[2], value=temp)
+output.to_csv(sys.argv[1], index=False)
