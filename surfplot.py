94f724a3b83b793f3ea25e887b20d5d5f3afd6a5
yoen <jendral.hxr@gmail.com>
2023-07-18 15:51:17 +0900

    plot only half the FFT response

--- surfplot.py
+++ surfplot.py
@@ -16,7 +16,7 @@ os.chdir("D://abdur//fft") # where you put the CSV files of FFT response and rat
 column_name= "finger_ratio"
 
 sweep= np.arange(1,101,1)
-freq=np.arange(0,500,500/2996)
+freq=np.arange(0,500/2,500/2996) #only half to show is okay
 #output.insert(loc=len(output.columns), column="freq", value=freq)
 
 for i in range(1,101):
@@ -27,10 +27,12 @@ for i in range(1,101):
     #print(tee.shape)
 
 response= output.to_numpy()
+response= response[0:freq.shape[0], 0:]
 
 sweep, freq = np.meshgrid(sweep, freq)
+
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-ax.set_ylim(0,250)
+#ax.set_ylim(0,250)
 surf = ax.plot_surface(sweep, freq, response, cmap=cm.jet, linewidth=0, antialiased=False)
 fig.colorbar(surf, shrink=0.5, aspect=5)
 
