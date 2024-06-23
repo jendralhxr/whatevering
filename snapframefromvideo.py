7d65eff6c9706c2602ce6c7d1287e1f6c1518d6b
jendralhxr <jendral.hxr@gmail.com>
2024-06-23 21:58:37 +0900

    frame number

--- snapframefromvideo.py
+++ snapframefromvideo.py
@@ -5,6 +5,7 @@ import os
 import sys
 
 cap = cv2.VideoCapture(sys.argv[1])
+videoname, ext= os.path.splitext(sys.argv[1])
 cap.set(cv2.CAP_PROP_POS_FRAMES, float(sys.argv[2]))
 ret, frame = cap.read()
-cv2.imwrite(sys.argv[3], frame)
+cv2.imwrite(videoname+sys.argv[2]+'.png', frame)
