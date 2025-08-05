5ad88ae5c3dde6448f39e3f5e12d7a09e570126e
jendralhxr <jendral.hxr@gmail.com>
2025-08-05 12:20:06 +0700

    push notify-send

--- swapaudio.sh
+++ swapaudio.sh
@@ -1,47 +1,33 @@
 #!/bin/bash
 
-# PulseAudio swap toggle script
-# Requires: pactl (or pacmd), PulseAudio
+# PulseAudio swap toggle script with GUI notifications
+# Dependencies: pactl, notify-send (usually provided by libnotify)
 
-# Name of the remapped sink
 SWAP_SINK_NAME="swapped"
-
-# Get the current default sink name
 DEFAULT_SINK=$(pactl info | awk -F': ' '/Default Sink/ {print $2}')
 
-# Check if the swapped sink is currently default
 if [[ "$DEFAULT_SINK" == "$SWAP_SINK_NAME" ]]; then
-    echo "Reverting to original sink..."
-
-    # Get the original sink (first one that is NOT the swapped sink)
+    # Revert to original sink
     ORIGINAL_SINK=$(pactl list short sinks | awk -v swap="$SWAP_SINK_NAME" '$2 != swap {print $2; exit}')
-
-    # Set it as default
     pactl set-default-sink "$ORIGINAL_SINK"
 
-    # Move all playing streams to the original sink
     for input in $(pactl list short sink-inputs | awk '{print $1}'); do
         pactl move-sink-input "$input" "$ORIGINAL_SINK"
     done
 
-    # Unload swapped sink module
     pactl unload-module module-remap-sink
 
-    echo "Audio channels set to normal (Left–Right)"
+    notify-send "🔊 Audio Channels" "Restored to normal (Left–Right)"
 else
-    echo "Swapping audio channels..."
-
-    # Load remapped swapped sink
+    # Swap left/right channels
     pactl unload-module module-remap-sink 2>/dev/null
     pactl load-module module-remap-sink sink_name=$SWAP_SINK_NAME master="$DEFAULT_SINK" channels=2 master_channel_map=right,left channel_map=left,right
 
-    # Set as default sink
     pactl set-default-sink "$SWAP_SINK_NAME"
 
-    # Move all playing streams to the swapped sink
     for input in $(pactl list short sink-inputs | awk '{print $1}'); do
         pactl move-sink-input "$input" "$SWAP_SINK_NAME"
     done
 
-    echo "Audio channels swapped (Left ⇄ Right)"
+    notify-send "🔄 Audio Channels" "Swapped (Left ⇄ Right)"
 fi
